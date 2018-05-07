6fd7cd06e4d4db35d38db2c120261768a26c2257
Snacky98 <38869697+Snacky98@users.noreply.github.com>
2018-05-07 01:40:05 -0700

    Started on a prototype for main

--- src/generator/Main.java
+++ src/generator/Main.java
@@ -1,8 +1,24 @@
 package generator;
 
+import java.util.*;
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        List<Item> itemList = new LinkedList<Item>();
 
+        Scanner listReader = null;
+        try {
+            File list = new File("The Lists - Base Sheet.tsv");
+            listReader = new Scanner(list);
+        } catch (FileNotFoundException e) {
+            System.out.println("Sorry, could not find the source file to get the pre built items from.");
+            System.exit(0);
+        }
+
+        while(listReader.hasNextLine()){
+            String newLine = listReader.nextLine();
+        }
     }
 }
